5694822a3c6bae0ca71f15170dee4ecf7c1eb8c4
Kris Chase <kris@mehh.org>
2020-03-24 23:26:33 -0700

    remove readonly attribute from form

--- src/components/Contribute.js
+++ src/components/Contribute.js
@@ -6,16 +6,17 @@ import ContributeForm from './ContributeForm';
 
 export default class Contribute extends React.Component {
     render() {
-        return (
-            <section className="subscribe contribute">
-              <div className="inner">
-                <h2 className="subscribe-title">Have something to add?</h2>
-                <p className="subscribe-text">
-                  We're always looking to expand this list, feel free to submit links below!
-                </p>
-                <ContributeForm {...this.props} />
-              </div>
-            </section>
-        );
+      return true;
+      // return (
+        //     <section className="subscribe contribute">
+        //       <div className="inner">
+        //         <h2 className="subscribe-title">Have something to add?</h2>
+        //         <p className="subscribe-text">
+        //           We're always looking to expand this list, feel free to submit links below!
+        //         </p>
+        //         <ContributeForm {...this.props} />
+        //       </div>
+        //     </section>
+        // );
     }
 }
